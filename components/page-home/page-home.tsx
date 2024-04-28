45915904662397b35775b104b6cbf3fdac6552a2
Yuri Gii <abovezenith@gmail.com>
2024-04-28 18:39:36 +0200

    nft data to const

--- components/page-home/page-home.tsx
+++ components/page-home/page-home.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { VStack, Flex, Box } from "@chakra-ui/react";
 import { Page } from "components/app/page";
-import { base } from "wagmi/chains";
 import { EVM_RMRK_CONTRACTS } from "lib/evm/constants";
 import { NftRenderer } from "components/common/nft-renderer";
+import { EVM_NETWORK_KEYS } from "lib/app/network-protocol-mapping";
+import { getActiveChainFromNetworkKey } from "lib/evm/utils";
+
+const MockLoadData = {
+  network: EVM_NETWORK_KEYS.base,
+  collectionId: EVM_RMRK_CONTRACTS.base.me,
+  tokenId: BigInt(51),
+};
 
 export const PageHome = () => (
   <Page data-name="page-home">
     <VStack align={"center"} justify={"center"} w={"100%"} h={"100vh"}>
       <Box w={"500px"} h={"500px"}>
         <NftRenderer
-          chainId={base.id}
-          contractAddress={EVM_RMRK_CONTRACTS.base.me}
-          tokenId={BigInt(51)}
+          chainId={getActiveChainFromNetworkKey(MockLoadData.network).id}
+          contractAddress={MockLoadData.collectionId}
+          tokenId={MockLoadData.tokenId}
           loader={
             <Flex align={"center"} justify={"center"}>
               Loading...
